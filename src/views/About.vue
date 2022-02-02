fb75e70338008edebd33c7eeca167be45e30b691
Simamkele21 <simamkelejanuary@gmail.com>
2022-02-02 15:37:10 +0200

    final push

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-<h1>About</h1>
+  <h1>About</h1>
   <div class="container bootstrap snippets bootdey">
     <div class="profile card">
       <div class="profile-body">
@@ -106,7 +106,56 @@ export default {
 
   data() {
     return {
-      Testimonials: [],
+      Testimonials: [
+        {
+          Description:
+            "He is a humble,kind and caring person who always does what is right, very dedicated to what he does and always trys to help others. Simamkele easly addapts to new situations and thinks quickly on his feet.To add to that he does not shy away from hard work and always puts his best foot foward",
+          id: 1,
+          img: "https://i.postimg.cc/kMQFYYXf/Na-aim1.jpg",
+          Name: "Na-aim Fredericks",
+          title: "Colleague",
+        },
+        {
+          Description:
+            "Simamkele is a hardworking person who always seeks help when stuck whick makes him a goal-driven person.",
+          id: 2,
+          img: "https://i.postimg.cc/kXBTDFyC/lilitha.jpg",
+          Name: "Lilitha Ngele",
+          title: "Colleague",
+        },
+        {
+          Description:
+            "Great humor and great person to work with. Always has a smile on his face and always works hard to finish the task at hand.",
+          id: 3,
+          img: "https://i.postimg.cc/9f73THtW/Muneeb3.jpg",
+          Name: "Mogamat Muneeb Davids",
+          title: "Colleague",
+        },
+        {
+          Description:
+            "Simamkele, the little man with big ideas. A very determined individual. You can always count on him to deliver good work. You Won't regret working with him!",
+          id: 4,
+          img: "https://i.postimg.cc/VNcgGyPs/Tursha.png",
+          Name: "Tursha Arendse",
+          title: "Colleague",
+        },
+        {
+          Description:
+            "Simamkele is a personable and easy to work with. He would be a valuable addition to your any coding team due to his personality.",
+          id: 5,
+          img: "https://i.postimg.cc/pdTrtrHs/Godwin.jpg",
+          Name: "Godwin Dzapatsva",
+          title: "Head of Curriculum and Learning",
+        },
+        {
+          Description:
+            "Simamkele is a bright individual. He analyses and continously finds ways to improve on himself and his technical abilities.",
+          id: 6,
+          img: "https://i.postimg.cc/Y2m49vmV/Alex.jpg",
+          Name: "Alex Sexwale",
+          title: "Senior Full-stack developer",
+        },
+      ],
     };
   },
   mounted() {
@@ -184,4 +233,6 @@ li a {
   padding-left: 200px;
   padding-right: 200px;
 }
+
+@media screen and (min-width: 100px) and (max-width: 799px) 
 </style>
--- src/views/Projects/Projects.vue
+++ src/views/Projects/Projects.vue
@@ -53,7 +53,72 @@ export default {
   },
   data() {
     return {
-      Projects: [],
+      Projects: [
+        {
+          img: "https://i.postimg.cc/ZRWhVvKX/project.png",
+          title: "MOCK PROJECT",
+          id: 1,
+          category: "HTML & CSS",
+          source: "https://github.com/simamkele21/project",
+          live: "https://eloquent-babbage-f22814.netlify.app/",
+        },
+        {
+          img: "https://i.postimg.cc/9f8hdYVG/onlinestore.png",
+          title: "ONLINE STORE",
+          id: 2,
+          category: "HTML & CSS",
+          source: "https://github.com/simamkele21/project",
+          live: "https://agitated-clarke-7afb9c.netlify.app/",
+        },
+        {
+          img: "https://i.postimg.cc/ZY1JsHNH/mediaq.png",
+          title: "MEDIA QUERIES",
+          id: 3,
+          category: "HTML & CSS",
+          source: "https://github.com/simamkele21/Media-Queries",
+          live: "https://gallant-wright-e119ca.netlify.app/",
+        },
+        {
+          img: "https://i.postimg.cc/sgJyXRNH/fc.png",
+          title: "Flipcards",
+          id: 4,
+          category: "HTML & CSS",
+          source: "https://github.com/simamkele21/flipcards",
+          live: "https://stoic-shannon-5c2e3b.netlify.app/",
+        },
+        {
+          img: "https://i.postimg.cc/02jSjGxR/Screenshot-from-2022-02-01-09-38-05.png",
+          title: "Box Model",
+          id: 5,
+          category: "HTML & CSS",
+          source: "https://github.com/simamkele21/boxmodel",
+          live: "https://keen-ptolemy-759e53.netlify.app/",
+        },
+        {
+          img: "https://i.postimg.cc/DZFSQLXg/Screenshot-from-2022-02-01-09-09-01.png",
+          title: "Reaction timer",
+          id: 6,
+          category: "Vue",
+          source: "https://github.com/simamkele21/Reaction-timer",
+          live: "https://awesome-leakey-bcfbbb.netlify.app/",
+        },
+        {
+          img: "https://i.postimg.cc/xdH2fZqx/Screenshot-from-2022-02-01-09-21-33.png",
+          title: "Kanye West API",
+          id: 7,
+          category: "Javascript",
+          source: "https://codepen.io/simamkele21/pen/vYejjYr",
+          live: "https://codepen.io/simamkele21/pen/vYejjYr",
+        },
+        {
+          img: "https://i.postimg.cc/ryHh8NR3/Screenshot-from-2022-02-01-10-14-24.png",
+          title: "Embeding",
+          id: 8,
+          category: "Javascript",
+          source: "https://codepen.io/simamkele21/pen/WNOVwjO",
+          live: "https://codepen.io/simamkele21/pen/WNOVwjO",
+        },
+      ],
     };
   },
   mounted() {
@@ -83,4 +148,6 @@ export default {
   width: 300px;
   height: 300px;
 }
+
+@media screen and (min-width: 100px) and (max-width: 799px);
 </style>
